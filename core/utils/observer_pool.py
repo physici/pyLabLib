fd7a9d54f6ff45eb616fd36fa4ee195b964a71a0
Alexey Shkarin <alex.shkarin@gmail.com>
2017-11-18 13:47:27 +0300

    Added missing documentation for some core.utils modules.

--- core/utils/observer_pool.py
+++ core/utils/observer_pool.py
@@ -1,9 +1,22 @@
+"""
+A simple observer pool (notification pool) implementeation.
+"""
+
 import collections
 from . import general, funcargparse
 
 _depends_local=[".general"]
 
 class ObserverPool(object):
+    """
+    An observer pool.
+
+    Stores notification functions (callbacks), and calls them whenever :meth:`notify` is called.
+    The callbacks can have priority (higher prioirity ones are called first) and tags (observer is only called if the notifying tag is among its tags).
+
+    Args:
+        expand_tuple(bool): if ``True`` and the notification value is a tuple, treat it as an argument list for the callback functions.
+    """
     def __init__(self, expand_tuple=True):
         self._observers={}
         self._expand_tuple=expand_tuple
@@ -11,6 +24,17 @@ class ObserverPool(object):
     _names_generator=general.NamedUIDGenerator(thread_safe=True)
     Observer=collections.namedtuple("Observer",["tags","callback","priority"])
     def add_observer(self, callback, name=None, tags=None, priority=0):
+        """
+        Add the observer callback.
+
+        Args:
+            callback(callable): callback function; takes at least one argument (notification tag), and possible more depending on the notification value.
+            name(str): stored callback name; by default, a unique name is auto-generated
+            tags(list or None): list of tags for this obserever (it is called only of the :meth:`notify` function tag is in this list); by default, all tags are accepted
+            priority(int): callback priority; higher priority callback are invoked first.
+        Returns:
+            callback name (equal to `name` if supplied; an automatically generated name otherwise).
+        """
         if name is None:
             name=self._names_generator("observer")
         if tags is not None:
@@ -18,6 +42,7 @@ class ObserverPool(object):
         self._observers[name]=self.Observer(tags,callback,priority)
         return name
     def remove_observer(self, name):
+        """Remove the observer callback with the given name."""
         del self._observers[name]
     
     def _call_observer(self, callback, tag, value):
@@ -25,7 +50,13 @@ class ObserverPool(object):
             return callback(tag,*value)
         else:
             return callback(tag,value)
-    def notify(self, tag, value=None):
+    def notify(self, tag, value=()):
+        """
+        Notify the obserevers by calling their callbacks.
+
+        Return a dictionary of the callback results.
+        By default the value is an empty tuple: for ``expand_tuple==True`` this means that only one argument (`tag`) is passed to the cakkbacks.
+        """
         to_call=[]
         for n,o in self._observers.items():
             if (o.tags is None) or (tag in o.tags):
